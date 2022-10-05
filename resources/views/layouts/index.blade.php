8c909bd0ce02c40b9baad32facdb4d24760a9a8a
= <eliasvalenzuela165@gmail.com>
2022-10-04 23:53:30 -0300

    feat : configure layouts base

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+       
+    </head>
+    <body class="antialiased">
+       @include('shared.navbar')
+      
+       <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            @yield('content')
+        </div>
+      </section>
+    </body>
+</html>
