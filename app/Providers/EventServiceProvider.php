65ca49d2a39d0173de550b485253eef08fb510b9
= <eliasvalenzuela165@gmail.com>
2022-10-05 22:51:06 -0300

    feat : configure listen and event for create and update onwer

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\CreatedCar;
+use App\Events\UpdateCar;
+use App\Listeners\CreatedCarListener;
+use App\Listeners\UpdateCarListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +22,8 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        CreatedCar::class => [CreatedCarListener::class],
+        UpdateCar::class => [UpdateCarListener::class]
     ];
 
     /**
